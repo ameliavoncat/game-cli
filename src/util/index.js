c1dad198f36bb8ba76ff65f98a9ad19e83ea466b
jeffreywescott <jeffrey@learnersguild.org>
2016-05-27 23:18:57 -0700

    add TODO comment about open-sourcing

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,5 @@
+// TODO: extract everything in `src/util` to its own OSS npm module
+
 export {default as addHelpOption} from './addHelpOption'
 export {default as findSubcommandDescriptor} from './findSubcommandDescriptor'
 export {default as parse} from './parse'
