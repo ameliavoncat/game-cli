6b7e7817b30ab169a017c12cb22fee944188e8a0
Trevor Little <trevor@learnersguild.org>
2016-06-16 14:49:08 -0400

    adding status and instructions to to log -r output

--- src/commands/__tests__/log.test.js
+++ src/commands/__tests__/log.test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 /* global expect, testContext */
-/* eslint-disable prefer-arrow-callback, no-unused-expressions */
+/* eslint-disable prefer-arrow-callback, no-unused-expressions, max-nested-callbacks */
 
 import nock from 'nock'
 
@@ -41,45 +41,114 @@ describe(testContext(__filename), function () {
         })
     })
 
-    it('prints all the queations when not given a number', function () {
-      nock('http://game.learnersguild.test')
-        .post('/graphql')
-        .reply(200, {data: {
-          getRetrospectiveSurvey: {
-            questions: [
-              {
-                id: '99ede319-882f-4dc8-81e2-be43f891a111',
-                subjectType: 'player',
-                responseType: 'text',
-                responseIntructions: 'these are the instructions',
-                body: 'this is the question body',
-                subject: {
-                  id: '34278883-2e76-42b6-a8aa-fa74a1892f90',
-                  name: 'Rosemarie Kub',
-                  handle: 'mobile81'
-                }
-              },
-              {
-                id: '99ede319-882f-4dc8-81e2-be43f891a122',
-                subjectType: 'player',
-                responseType: 'text',
-                responseIntructions: 'these are the instructions',
-                body: 'this is the second question body',
-                subject: {
-                  id: '34278883-2e76-42b6-a8aa-fa74a1892faa',
-                  name: 'Trevor Little',
-                  handle: 'tlittle'
-                }
-              },
-            ]
-          }
-        }})
+    describe('log -r', function () {
+      describe('when survey in progress', function () {
+        beforeEach('mock game API', function () {
+          nock('http://game.learnersguild.test')
+            .post('/graphql')
+            .reply(200, {data: {
+              getRetrospectiveSurvey: {
+                questions: [
+                  {
+                    id: '99ede319-882f-4dc8-81e2-be43f891a111',
+                    subjectType: 'player',
+                    responseType: 'text',
+                    responseIntructions: 'these are the instructions',
+                    body: 'this is the question body',
+                    subject: {
+                      id: '34278883-2e76-42b6-a8aa-fa74a1892f90',
+                      name: 'Rosemarie Kub',
+                      handle: 'mobile81'
+                    },
+                    response: 'value',
+                  },
+                  {
+                    id: '99ede319-882f-4dc8-81e2-be43f891a122',
+                    subjectType: 'player',
+                    responseType: 'text',
+                    responseIntructions: 'these are the instructions',
+                    body: 'this is the second question body',
+                    subject: {
+                      id: '34278883-2e76-42b6-a8aa-fa74a1892faa',
+                      name: 'Trevor Little',
+                      handle: 'tlittle'
+                    },
+                    response: null,
+                  },
+                ]
+              }
+            }})
+        })
 
-      const {lgJWT, lgPlayer} = this
-      return this.invoke(['-r'], this.notify, {lgJWT, lgPlayer}).then(() => {
-        expect(this.notifications).to.match(/this is the question body/i)
-        expect(this.notifications).to.match(/this is the second question body/i)
-        expect(this.notifications).to.not.match(/these are the instructions/i)
+        it('prints all the questions without question instructions', function () {
+          const {lgJWT, lgPlayer} = this
+          return this.invoke(['-r'], this.notify, {lgJWT, lgPlayer}).then(() => {
+            expect(this.notifications[0]).to.match(/this is the question body/i)
+            expect(this.notifications[0]).to.match(/this is the second question body/i)
+            expect(this.notifications[0]).to.not.match(/these are the instructions/i)
+          })
+        })
+
+        it('prints survey instructions', function () {
+          const {lgJWT, lgPlayer} = this
+          return this.invoke(['-r'], this.notify, {lgJWT, lgPlayer}).then(() => {
+            expect(this.notifications[0]).to.match(/To log a reflection,/i)
+            expect(this.notifications[0]).to.match(/Then follow the instructions specified in the question to answer./i)
+          })
+        })
+
+        it('prints status information', function () {
+          const {lgJWT, lgPlayer} = this
+          return this.invoke(['-r'], this.notify, {lgJWT, lgPlayer}).then(() => {
+            expect(this.notifications[0]).to.match(/You have logged 1\/2/i)
+          })
+        })
+      })
+
+      describe('when survey complete', function () {
+        beforeEach('mock game API', function () {
+          nock('http://game.learnersguild.test')
+            .post('/graphql')
+            .reply(200, {data: {
+              getRetrospectiveSurvey: {
+                questions: [
+                  {
+                    id: '99ede319-882f-4dc8-81e2-be43f891a111',
+                    subjectType: 'player',
+                    responseType: 'text',
+                    responseIntructions: 'these are the instructions',
+                    body: 'this is the question body',
+                    subject: {
+                      id: '34278883-2e76-42b6-a8aa-fa74a1892f90',
+                      name: 'Rosemarie Kub',
+                      handle: 'mobile81'
+                    },
+                    response: 'value',
+                  },
+                  {
+                    id: '99ede319-882f-4dc8-81e2-be43f891a122',
+                    subjectType: 'player',
+                    responseType: 'text',
+                    responseIntructions: 'these are the instructions',
+                    body: 'this is the second question body',
+                    subject: {
+                      id: '34278883-2e76-42b6-a8aa-fa74a1892faa',
+                      name: 'Trevor Little',
+                      handle: 'tlittle'
+                    },
+                    response: 'value',
+                  },
+                ]
+              }
+            }})
+        })
+
+        it('prints a "completed" message', function () {
+          const {lgJWT, lgPlayer} = this
+          return this.invoke(['-r'], this.notify, {lgJWT, lgPlayer}).then(() => {
+            expect(this.notifications[0]).to.match(/You've completed 100%/i)
+          })
+        })
       })
     })
 
@@ -97,14 +166,15 @@ describe(testContext(__filename), function () {
               id: '34278883-2e76-42b6-a8aa-fa74a1892f90',
               name: 'Rosemarie Kub',
               handle: 'mobile81'
-            }
+            },
+            response: 'value',
           }
         }})
 
       const {lgJWT, lgPlayer} = this
       return this.invoke(['-rq', '1'], this.notify, {lgJWT, lgPlayer}).then(() => {
-        expect(this.notifications).to.match(/this is the question body/i)
-        expect(this.notifications).to.match(/these are the instructions/i)
+        expect(this.notifications[0]).to.match(/this is the question body/i)
+        expect(this.notifications[0]).to.match(/these are the instructions/i)
       })
     })
 
